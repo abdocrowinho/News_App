e15a6ce3753d9bf57fd04addd638ec851a5c624c
abdocrowinho <ahlyua2@gmail.com>
2024-09-03 04:58:07 +0200

    ++feat/Logic/MainActivity

--- app/src/main/java/com/example/newsapp/UI/activitis/homeActivity/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/UI/activitis/homeActivity/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.newsapp.UI.activitis.homeActivity
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.newsapp.R
+import com.example.newsapp.bases.BaseActivity
+import com.example.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val naHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = naHostFragment.navController
         installSplashScreen()
-    setContentView(R.layout.activity_main)
+        setContentView(binding?.root)
+
 
     }
-
 
 }
--- app/src/main/java/com/example/newsapp/bases/BaseActivity.kt
+++ app/src/main/java/com/example/newsapp/bases/BaseActivity.kt
@@ -1,17 +1,21 @@
 package com.example.newsapp.bases
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.DataBindingUtil
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseActivity<Vb :ViewBinding > : AppCompatActivity() {
-  private var _binding :Vb ?=null
-    val binding :Vb get()  = _binding!!
+abstract class BaseActivity<Vb : ViewBinding> : AppCompatActivity() {
+    private var _binding: Vb? = null
+    val binding: Vb get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        _binding = DataBindingUtil.setContentView(this,getLayoutId())
+        installSplashScreen()
+        _binding = DataBindingUtil.setContentView(this, getLayoutId())
 
     }
 
@@ -20,6 +24,76 @@ abstract class BaseActivity<Vb :ViewBinding > : AppCompatActivity() {
         _binding = null
     }
 
+    fun showDialog(
+        message: String,
+        posText: String? = null,
+        negText: String? = null,
+        posCallBack: onDialogClick? = null,
+        negCallBack: onDialogClick? = null,
+        isCancelable : Boolean = true
 
-    abstract  fun getLayoutId():Int
+    ) {
+        val builder = AlertDialog.Builder(this)
+            .setMessage(message)
+        posText?.let {
+            builder.setPositiveButton(
+                posText
+            ) { dialog, i ->
+                dialog.dismiss()
+                posCallBack?.onDialogClick()
+
+
+            }
+        }
+        negText?.let {
+            builder.setNegativeButton(
+                negText
+            ) { dialog, i ->
+                dialog.dismiss()
+                negCallBack?.onDialogClick()
+            }
+        }
+        builder.setCancelable(isCancelable)
+
+
+    }
+    fun showDialog(
+       @StringRes message: Int,
+       @StringRes posText: Int? = null,
+       @StringRes negText: Int? = null,
+        posCallBack: onDialogClick? = null,
+        negCallBack: onDialogClick? = null,
+        isCancelable : Boolean = true
+
+    ) {
+        val builder = AlertDialog.Builder(this)
+            .setMessage(message)
+        posText?.let {
+            builder.setPositiveButton(
+                posText
+            ) { dialog, i ->
+                dialog.dismiss()
+                posCallBack?.onDialogClick()
+
+
+            }
+        }
+        negText?.let {
+            builder.setNegativeButton(
+                negText
+            ) { dialog, i ->
+                dialog.dismiss()
+                negCallBack?.onDialogClick()
+            }
+        }
+        builder.setCancelable(isCancelable)
+
+
+    }
+
+    fun interface onDialogClick {
+        fun onDialogClick(): DialogInterface
+    }
+
+    abstract fun getLayoutId(): Int
 }
